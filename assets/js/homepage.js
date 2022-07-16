42b7a61b8a7b6a5f42ba68faf23e1ef3dda171c2
millerd <millerdicarlo@gmail.com>
2022-07-16 13:27:56 -0400

    fetch API and HTTP requests set up

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,38 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+//Fetch function
+var getUserRepos = function(user) {
+    // format github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make request to url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        displayRepos(data, user);
+      });
+    });
+  };
+
+  // form submission function
+  var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    // get value from input element
+    var username = nameInputEl.value.trim();
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+  };
+
+// Function to display usernames and repos
+  var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
   };
   
-  getUserRepos();
+  userFormEl.addEventListener("submit", formSubmitHandler);
